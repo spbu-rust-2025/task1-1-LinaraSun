515c177089e05f983b3040a3d2b14a00daaaad1c
Linara Sankubaeva <linarasankubaeva55@mail.ru>
2025-10-24 12:23:59 +0300

    Made a program that takes two numbers from stdin and delivers theirs sum to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::io;
 
+fn main() {
+	let mut str1 = String::new();
+	let mut str2 = String::new();
+
+	io::stdin()
+		.read_line(&mut str1)
+		.expect("Failed input");
+
+	io::stdin()
+		.read_line(&mut str2)
+		.expect("Failed input");
+
+	let num1: i32 = str1.trim().parse().expect("Please type a number!");
+	let num2: i32 = str2.trim().parse().expect("Please type a number!");
+
+	let sum = num1 + num2;
+	println!("{}", sum);
 }
